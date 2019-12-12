8a2a7877a4b7f807ccea539cdae26625d66175cc
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2019-12-11 23:32:09 -0800

    tutorial/basics
    Learning the basics of C#

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Variables
+namespace TypeConversion
 {
     class Program
     {
@@ -27,8 +27,42 @@ namespace Variables
             //Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
 
             // declaring a constant
-            const float Pi = 3.14f;
+            //const float Pi = 3.14f;
 
+            //byte b = 1;
+            //int i = b;
+            //Console.WriteLine(i);
+            // compiles correctly above
+
+            //int i = 1;
+            //byte b = (byte) i;
+            //Console.WriteLine(b);
+            // compiles correctly with the (byte)
+
+            //int i = 1000;
+            //byte b = (byte)i;
+            //Console.WriteLine(b);
+            //// will display 232 because that is the limit of the byte
+            ///
+            //    var number = "1234";
+            //    int i = Convert.ToInt32(number);
+            //    Console.WriteLine(i);
+            try
+            {
+                //var number = "1234";
+                //byte b = Convert.ToByte(number);
+                //Console.WriteLine(b);
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+            }
+
+
+           
         }
     }
 }
